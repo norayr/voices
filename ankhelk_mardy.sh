758ca11942a86aca15695556bd6902f6ebd80a36
norayr <norayr@arnet.am>
2018-10-31 18:51:36 +0400

    ankhelk

--- ankhelk_mardy.sh
+++ ankhelk_mardy.sh
@@ -1,2 +1,4 @@
 link="https://www.youtube.com/watch?v=Um3oahZRUWY"
-youtube-dl -x --audio-format=mp3 --audio-quality=0 $link
+scriptname0=`basename "$0"`
+scriptname="${scriptname0%.*}"
+youtube-dl -x --audio-format=mp3 --audio-quality=0 --output ${scriptname}.mp3 $link
